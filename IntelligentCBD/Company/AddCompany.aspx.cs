1edb67e73b6ac902e4c382ce1c74255f91263ace
Dallas <dallas2k@163.com>
2016-05-30 17:36:35 +0800

    进行了一些测试，改进了一些问题

--- IntelligentCBD/Company/AddCompany.aspx.cs
+++ IntelligentCBD/Company/AddCompany.aspx.cs
@@ -27,6 +27,29 @@ namespace IntelligentCBD.Company
             Response.Write(cc.RegistrationDate);
             */
             //cc.ID=Guid.NewGuid();
+
+            cc.CompanyName = TextBox_CompanyName.Text;
+            cc.Capital = t.CapitalString2Long(TextBox_Capital.Text, DropDownList_Units.SelectedValue);
+            cc.Industry = TextBox_Industry.Text;
+            //此处需如此处理面积
+            int area;
+            int.TryParse(TextBox_Area.Text, out area);
+            cc.Area = area;
+            cc.RoomNum = TextBox_RoomNum.Text;
+            cc.Address = TextBox_Address.Text;
+            cc.Vector = DropDownList_Vector.SelectedValue;
+            cc.Introduction = TextBox_Introduction.Text;
+            cc.BusinessScope = TextBox_BusinessScope.Text;
+            cc.RegisteredAddress = TextBox_RegisteredAddress.Text;
+            cc.RegistrationDate = TextBox_RegistrationDate.Text;
+            cc.Contact = TextBox_Contact.Text;
+            cc.Phone = TextBox_Phone.Text;
+            cc.Email = TextBox_Email.Text;
+            cc.QQ = TextBox_QQ.Text;
+            cc.Content = elm1.InnerText;
+
+            //考虑到数据库存储问题，此处不使用Server.HtmlEncode方法，因为会增大数据库存储容量造成溢出错误；
+            /*
             cc.CompanyName = Server.HtmlEncode(TextBox_CompanyName.Text);
             cc.Capital = t.CapitalString2Long(TextBox_Capital.Text, DropDownList_Units.SelectedValue);
             cc.Industry = Server.HtmlEncode(TextBox_Industry.Text);
@@ -37,7 +60,7 @@ namespace IntelligentCBD.Company
             cc.RoomNum = Server.HtmlEncode(TextBox_RoomNum.Text);
             cc.Address = Server.HtmlEncode(TextBox_Address.Text);
             cc.Vector = DropDownList_Vector.SelectedValue;
-            cc.Introduction =  Server.HtmlEncode( TextBox_Introduction.Text);
+            cc.Introduction =  Server.HtmlEncode(TextBox_Introduction.Text);
             cc.BusinessScope = Server.HtmlEncode(TextBox_BusinessScope.Text);
             cc.RegisteredAddress = Server.HtmlEncode(TextBox_RegisteredAddress.Text);
             cc.RegistrationDate = TextBox_RegistrationDate.Text;
@@ -47,6 +70,14 @@ namespace IntelligentCBD.Company
             cc.QQ = Server.HtmlEncode(TextBox_QQ.Text);
             cc.Content = elm1.InnerText;
             Response.Write(cc.Content);
+            /*
+            
+            /*测试
+            //cc.Address = TextBox_Address.Text;
+            LabelSucceed.Text = Server.HtmlDecode(cc.Address);
+            TextBox1.Text = Server.HtmlDecode(cc.Address);
+            Response.Write(Server.HtmlDecode(cc.Address));
+            */
         }
     }
 }
